e6668e61fd17a3771025bff629d1fad2b28bccbb
tsanta-niaina-MT <132874156+tsanta-niaina-MT@users.noreply.github.com>
2023-11-19 18:37:51 +0100

    commit change

---
+++ libraryManagementApplication.java
@@ -0,0 +1,8 @@
+package libraryManagement;
+
+public class libraryManagementApplication {
+        public static void main(String[] args) {
+            System.out.printf("Hello every one!");
+
+    }
+}
